143d8f30b27b2f4825612c26122fc4b6ee4a3276
trantuankiet123 <trantuankiet11884@gmail.com>
2023-08-09 22:11:11 +0700

    valid email existed, create ctrl,route login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,12 +8,39 @@ const register = asyncHandler(async (req, res) => {
       sucess: false,
       mes: "Missing values",
     });
-  const response = await User.create(req.body);
-  return res.status(200).json({
-    sucess: response ? true : false,
-  });
+
+  const user = await User.findOne({ email });
+  if (user) throw new Error("Email has existed!");
+  else {
+    const newUser = await User.create(req.body);
+    return res.status(200).json({
+      sucess: newUser ? true : false,
+      mes: newUser ? "Register is successfully" : "Something wrongs....",
+    });
+  }
+});
+
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({
+      sucess: false,
+      mes: "Missing values",
+    });
+
+  const response = await User.findOne({ email });
+  if (response && (await response.isCorrectPassword(password))) {
+    const { password, role, ...userData } = response.toObject();
+    return res.status(200).json({
+      sucess: true,
+      userData,
+    });
+  } else {
+    throw new Error("Invalid credentials!!!");
+  }
 });
 
 module.exports = {
   register,
+  login,
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,5 +3,6 @@ const ctrls = require("../controllers/user.js");
 const router = require("express").Router();
 
 router.post("/register", ctrls.register);
+router.post("/login", ctrls.login);
 
 module.exports = router;
